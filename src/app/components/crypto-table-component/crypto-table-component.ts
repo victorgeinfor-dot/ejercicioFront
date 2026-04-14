f8c4e31ec549b65b65658eeb3e68dbe0fb0b4d4c
victor <victor.geinfor@gmail.com>
2026-04-14 17:46:17 +0200

    Commit Componentes Repasados

--- src/app/components/crypto-table-component/crypto-table-component.ts
+++ src/app/components/crypto-table-component/crypto-table-component.ts
@@ -1,9 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CryptoApiService } from '../../services/crypto-api-service';
 import { Crypto } from '../../models/crypto.model';
-import { Observable } from 'rxjs';
-import { HeaderComponent } from '../header-component/header-component';
 
 @Component({
   selector: 'app-crypto-table-component',
